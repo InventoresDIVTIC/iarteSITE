e1cf6f42eb48ed0c43a2d206b386fbbc2d47fecb
SnowTrash <juan.vargas2962@alumnos.udg.mx>
2023-10-14 20:16:28 -0600

    updt_routes
    corrigiendo rutas

--- galeria/iart-gallery/api/api_galeria_csv.php
+++ galeria/iart-gallery/api/api_galeria_csv.php
@@ -1,7 +1,7 @@
 <?php
 
 // Define the path to your CSV file
-$csvFilePath = 'data/registro.csv'; // Update with the correct file name
+$csvFilePath = '../data/registro.csv'; // Update with the correct file name
 
 // Check if the CSV file exists
 if (!file_exists($csvFilePath)) {
@@ -42,7 +42,7 @@ while (($row = fgetcsv($csvFile)) !== false) {
             $imageFileName = str_replace('./img/', '', $imageFileName);
 
             // Build the full path to the image file
-            $imagePath = 'img/' . $imageFolder . $imageFileName;
+            $imagePath = '../img/' . $imageFolder . $imageFileName;
 
             // Check if the image file exists and is not empty
             if (file_exists($imagePath)) {
