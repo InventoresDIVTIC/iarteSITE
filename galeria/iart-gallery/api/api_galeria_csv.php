7a14f2787ca3a1cf1d71ea98ba907eb56246cf00
SnowTrash <juan.vargas2962@alumnos.udg.mx>
2023-10-16 15:56:38 -0600

    api_fix
    modificando la api

--- galeria/iart-gallery/api/api_galeria_csv.php
+++ galeria/iart-gallery/api/api_galeria_csv.php
@@ -1,92 +1,72 @@
 <?php
+
 // Define the path to your CSV file
 $csvFilePath = '../data/registro.csv'; // Update with the correct file name
-$jsonCachePath = 'cached_data.json';
 
-// Check if the cached JSON data exists and is up-to-date
-if (file_exists($jsonCachePath) && filemtime($jsonCachePath) >= filemtime($csvFilePath)) {
-    // Serve the cached JSON data
-    header('Content-Type: application/json');
-    readfile($jsonCachePath);
-} else {
-    // Check if the CSV file exists
-    if (!file_exists($csvFilePath)) {
-        die("CSV file not found.");
+// Open the CSV file for reading
+$csvFile = fopen($csvFilePath, 'r');
+
+// obtener el header de los csv
+$headerRow = fgetcsv($csvFile);
+
+// Initialize an array to hold the paintings
+$paintings = [];
+
+while (($row = fgetcsv($csvFile)) !== false) {
+    // Initialize an array to hold multiple paintings for this row
+    $rowPaintings = [];
+
+    // Loop through the image fields (imagen1, imagen2, imagen3, imagen4)
+    for ($i = 1; $i <= 4; $i++) {
+        $imageField = 'imagen' . $i;
+        $descriptionField = 'descripcion' . $i;
+
+        // Determine which folder to use based on the image field
+        $imageFolder = ($i <= 2) ? 'Producto innovador/' : 'Interacción ciber-humana/';
+
+        // Get the image file name from the CSV row
+        $imageFileName = $row[array_search($imageField, $headerRow)];
+
+        // Remove the "./img/" part from the image path
+        $imageFileName = str_replace('./img/', '', $imageFileName);
+        // Build the full path to the image file
+        $imagePath = '../img/' . $imageFolder . $imageFileName;
+        // Read the image file content
+        $imageContent = file_get_contents($imagePath);
+        // Encode the image content as base64
+        $base64Image = base64_encode($imageContent);
+        // Create a painting object
+        $painting = [
+            "image_id" => $row[array_search('id_registro', $headerRow)] . $imageFileName,
+            "title" => $row[array_search($descriptionField, $headerRow)],
+            "artist_title" => $row[array_search('nombre', $headerRow)],
+            "image" => $base64Image,
+        ];
+        // Add the painting object to the row's paintings array
+        $rowPaintings[] = $painting;
     }
 
-    // Open the CSV file for reading
-    $csvFile = fopen($csvFilePath, 'r');
-
-    if (!$csvFile) {
-        die("Failed to open CSV file.");
-    }
-
-    // Initialize an array to hold the paintings
-    $paintings = [];
-
-    // Initialize an array to hold image data (base64-encoded images)
-    $imageData = [];
-
-    $headerRow = fgetcsv($csvFile);
-
-    while (($row = fgetcsv($csvFile)) !== false) {
-        for ($i = 1; $i <= 4; $i++) {
-            $imageField = 'imagen' . $i;
-            $descriptionField = 'descripcion' . $i;
-
-            $imageFileName = $row[array_search($imageField, $headerRow)];
-
-            if (!empty($imageFileName)) {
-                $imageFileName = str_replace('./img/', '', $imageFileName);
-                $imagePath = '../img/' . (($i <= 2) ? 'Producto innovador/' : 'Interacción ciber-humana/') . $imageFileName;
-
-                if (file_exists($imagePath)) {
-                    if (!isset($imageData[$imagePath])) {
-                        $imageContent = file_get_contents($imagePath);
-                        $imageData[$imagePath] = base64_encode($imageContent);
-                    }
-
-                    $painting = [
-                        "image_id" => $row[array_search('id_registro', $headerRow)] . $imageFileName,
-                        "title" => $row[array_search($descriptionField, $headerRow)],
-                        "artist_title" => $row[array_search('nombre', $headerRow)],
-                        "image" => $imageData[$imagePath],
-                    ];
-
-                    $paintings[] = $painting;
-                }
-            }
-        }
-    }
-
-    // Close the CSV file
-    fclose($csvFile);
-
-    // Add any additional processing or data manipulation here
-
-    // Add the last painting to the top of the array
-    array_unshift($paintings, end($paintings));
-
-    // Create copies and merge with the original paintings
-    $lastPainting = end($paintings);
-    $copies = [];
-
-    for ($i = 1; $i <= 8; $i++) {
-        $copiedPainting = $lastPainting;
-        $copiedPainting["image_id"] = $lastPainting["image_id"] . "_copy" . $i;
-        $copies[] = $copiedPainting;
-    }
-
-    $paintings = array_merge($copies, $paintings);
-
-    // Output the paintings in JSON format
-    $jsonData = json_encode(["data" => $paintings]);
-
-    // Cache the JSON data
-    file_put_contents($jsonCachePath, $jsonData);
-
-    // Serve the JSON data
-    header('Content-Type: application/json');
-    echo $jsonData;
+    // Add the row's paintings array to the main paintings array after reversing it
+    $paintings = array_merge($paintings, $rowPaintings);
 }
+
+// Get the last painting
+$lastPainting = end($paintings);
+
+// Add 8 copies of the last painting with different image_id values
+for ($i = 8; $i >= 1; $i--) {
+    $copiedPainting = $lastPainting;
+    $copiedPainting["image_id"] = $lastPainting["image_id"] . "_copy" . $i;
+    $copies[] = $copiedPainting;
+}
+
+// Merge the copies with the original paintings
+$paintings = array_merge($copies, $paintings);
+
+// Close the CSV file
+fclose($csvFile);
+
+// Output the paintings in JSON format
+echo json_encode(["data" => $paintings]);
+
 ?>
