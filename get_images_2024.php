6783b6e1f5b186a334ac3205308529626d411040
SnowTrash <juan.vargas2962@alumnos.udg.mx>
2024-05-21 19:02:35 -0600

    bk

--- get_images_2024.php
+++ get_images_2024.php
@@ -1,5 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
 include('conexion.php'); // Asegúrate de que este path es correcto.
 $conexion = conectar();
 
@@ -12,9 +11,6 @@ if ($resultado) {
     while ($fila = mysqli_fetch_assoc($resultado)) {
         if ($fila['imagen1'] != ''){
             $images[] = $fila['imagen1']; // Añadir cada nombre de imagen al array
-            
-            $new_prefix = '/galeria/iart-gallery/img/Producto innovador';
-            $images[] = str_replace('./img/', $new_prefix, $fila['imagen1']);
         }
     }
     // Codificar el array en formato JSON y enviarlo como respuesta
