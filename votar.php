c3b1ce1d134c67ce0254fe66f88b4b6f671da1a0
SnowTrash <juan.vargas2962@alumnos.udg.mx>
2024-09-19 16:53:50 -0600

    err
    correccion2

--- votar.php
+++ votar.php
@@ -16,13 +16,13 @@ if ($resultado) {
     while ($fila = mysqli_fetch_assoc($resultado)) {
         // Verificar y procesar imagen1 e imagen2 (Producto inovador)
         if ($fila['imagen1'] != '') {
-            $ruta1 = str_replace('./img/', './img/Producto inovador/', $fila['imagen1']);
+            $ruta1 = str_replace('./img/', './galeria/iart-gallery/img/Producto inovador/', $fila['imagen1']);
             if (file_exists($ruta1)) {
                 $images[] = array("ruta" => $ruta1, "prompt" => "Descripción del prompt para imagen1");
             }
         }
         if ($fila['imagen2'] != '') {
-            $ruta2 = str_replace('./img/', './img/Producto inovador/', $fila['imagen2']);
+            $ruta2 = str_replace('./img/', './galeria/iart-gallery/img/Producto inovador/', $fila['imagen2']);
             if (file_exists($ruta2)) {
                 $images[] = array("ruta" => $ruta2, "prompt" => "Descripción del prompt para imagen2");
             }
@@ -30,13 +30,13 @@ if ($resultado) {
 
         // Verificar y procesar imagen3 e imagen4 (Interacción ciber-humana)
         if ($fila['imagen3'] != '') {
-            $ruta3 = str_replace('./img/', './galeria/iart-galery/img/interaccion ciber-humana/', $fila['imagen3']);
+            $ruta3 = str_replace('./img/', './galeria/iart-gallery/img/interaccion ciber-humana/', $fila['imagen3']);
             if (file_exists($ruta3)) {
                 $images[] = array("ruta" => $ruta3, "prompt" => "Descripción del prompt para imagen3");
             }
         }
         if ($fila['imagen4'] != '') {
-            $ruta4 = str_replace('./img/', './galeria/iart-galery/img/interacción ciber-humana/', $fila['imagen4']);
+            $ruta4 = str_replace('./img/', './galeria/iart-gallery/img/interacción ciber-humana/', $fila['imagen4']);
             if (file_exists($ruta4)) {
                 $images[] = array("ruta" => $ruta4, "prompt" => "Descripción del prompt para imagen4");
             }
