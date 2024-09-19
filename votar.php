208f261435c7aa55268f111c01640714af10e511
SnowTrash <juan.vargas2962@alumnos.udg.mx>
2024-09-19 17:09:01 -0600

    fix2

--- votar.php
+++ votar.php
@@ -16,7 +16,7 @@ if ($resultado) {
     while ($fila = mysqli_fetch_assoc($resultado)) {
         // Verificar y procesar imagen1 e imagen2 (Producto inovador)
         if (!empty($fila['imagen1'])) {
-            $ruta1 = str_replace('./img/', './galeria/iart-gallery/img/Producto inovador/', $fila['imagen1']);
+            $ruta1 = str_replace('./img/', './galeria/iart-gallery/img/Producto innovador/', $fila['imagen1']);
             if (file_exists($ruta1)) {
                 $images[] = array("ruta" => $ruta1, "prompt" => "Descripción del prompt para imagen1");
             }
@@ -26,7 +26,7 @@ if ($resultado) {
         }
 
         if (!empty($fila['imagen2'])) {
-            $ruta2 = str_replace('./img/', './galeria/iart-gallery/img/Producto inovador/', $fila['imagen2']);
+            $ruta2 = str_replace('./img/', './galeria/iart-gallery/img/Producto innovador/', $fila['imagen2']);
             if (file_exists($ruta2)) {
                 $images[] = array("ruta" => $ruta2, "prompt" => "Descripción del prompt para imagen2");
             }
@@ -36,7 +36,7 @@ if ($resultado) {
 
         // Verificar y procesar imagen3 e imagen4 (Interacción ciber-humana)
         if (!empty($fila['imagen3'])) {
-            $ruta3 = str_replace('./img/', './galeria/iart-gallery/img/interaccion ciber-humana/', $fila['imagen3']);
+            $ruta3 = str_replace('./img/', './galeria/iart-gallery/img/Interacción ciber-humana/', $fila['imagen3']);
             if (file_exists($ruta3)) {
                 $images[] = array("ruta" => $ruta3, "prompt" => "Descripción del prompt para imagen3");
             }
@@ -45,7 +45,7 @@ if ($resultado) {
         }
 
         if (!empty($fila['imagen4'])) {
-            $ruta4 = str_replace('./img/', './galeria/iart-gallery/img/interacción ciber-humana/', $fila['imagen4']);
+            $ruta4 = str_replace('./img/', './galeria/iart-gallery/img/Interacción ciber-humana/', $fila['imagen4']);
             if (file_exists($ruta4)) {
                 $images[] = array("ruta" => $ruta4, "prompt" => "Descripción del prompt para imagen4");
             }
